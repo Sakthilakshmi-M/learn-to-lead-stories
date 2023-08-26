1038dea393c433fd9ee4f06db2fec5d958141869
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2023-08-26 18:54:52 +0530

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Learn 2 Lead</h1>
+      <ul className="nav-links">
+        <li><a href="">Home</a></li>
+        <li>About Us</li>
+        <li><a href="">Stories</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
